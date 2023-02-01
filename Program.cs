e30cda415f6ffdda2685372cbab61760ffc42b6e
JerenHicks <jeren.hicks@aviture.us.com>
2023-01-31 23:03:58 -0600

    Clean up.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,9 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Text.Json;
-
-
-class Program
+﻿class Program
 {
-    readonly static string serverFileLocation = @".\serverlist.json";
 
-    public static int Main(String[] args)
+    public static void Main(String[] args)
     {
         ServerCreation serverCreation = new ServerCreation();
         serverCreation.StartUp();
-        return 0;
     }
 }
